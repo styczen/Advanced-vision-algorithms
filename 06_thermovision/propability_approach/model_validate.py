1a7fa8c899a0cebebc1d929b7346823bbbfbafc2
Bartłomiej <styczenb@student.agh.edu.pl>
2019-04-08 09:32:23 +0200

    Add slow version of non-max suppression

--- 06_thermovision/propability_approach/model_validate.py
+++ 06_thermovision/propability_approach/model_validate.py
@@ -5,7 +5,7 @@ import os
 
 THRESHOLD = 40
 NR_SAMPLES = 39
-PROB_THRESHOLD = 230
+PROB_THRESHOLD = 240
 STRIDE = 5
 
 DIR = os.path.dirname(sys.argv[0])
@@ -70,6 +70,72 @@ def non_max_suppression_fast(boxes, overlapThresh):
     return boxes[pick].astype("int")
 
 
+#  Felzenszwalb et al.
+def non_max_suppression_slow(boxes, overlapThresh):
+    # if there are no boxes, return an empty list
+    if len(boxes) == 0:
+        return []
+
+    # initialize the list of picked indexes
+    pick = []
+
+    # grab the coordinates of the bounding boxes
+    x1 = boxes[:, 0]
+    y1 = boxes[:, 1]
+    x2 = boxes[:, 2]
+    y2 = boxes[:, 3]
+
+    # compute the area of the bounding boxes and sort the bounding
+    # boxes by the bottom-right y-coordinate of the bounding box
+    area = (x2 - x1 + 1) * (y2 - y1 + 1)
+    idxs = np.argsort(y2)
+
+    # keep looping while some indexes still remain in the indexes
+    # list
+    while len(idxs) > 0:
+        # grab the last index in the indexes list, add the index
+        # value to the list of picked indexes, then initialize
+        # the suppression list (i.e. indexes that will be deleted)
+        # using the last index
+        last = len(idxs) - 1
+        i = idxs[last]
+        pick.append(i)
+        suppress = [last]
+
+        # loop over all indexes in the indexes list
+        for pos in range(0, last):
+            # grab the current index
+            j = idxs[pos]
+
+            # find the largest (x, y) coordinates for the start of
+            # the bounding box and the smallest (x, y) coordinates
+            # for the end of the bounding box
+            xx1 = max(x1[i], x1[j])
+            yy1 = max(y1[i], y1[j])
+            xx2 = min(x2[i], x2[j])
+            yy2 = min(y2[i], y2[j])
+
+            # compute the width and height of the bounding box
+            w = max(0, xx2 - xx1 + 1)
+            h = max(0, yy2 - yy1 + 1)
+
+            # compute the ratio of overlap between the computed
+            # bounding box and the bounding box in the area list
+            overlap = float(w * h) / area[j]
+
+            # if there is sufficient overlap, suppress the
+            # current bounding box
+            if overlap > overlapThresh:
+                suppress.append(pos)
+
+        # delete all indexes from the index list that are in the
+        # suppression list
+        idxs = np.delete(idxs, suppress)
+
+        # return only the bounding boxes that were picked
+    return boxes[pick]
+
+
 cap = cv2.VideoCapture(DIR + '/../vid1_IR.avi')
 
 model = cv2.imread(DIR + '/dpm.png', cv2.IMREAD_GRAYSCALE).astype(np.float32)
@@ -84,7 +150,7 @@ h, w = frame_bgr.shape[:2]
 nr_frame = 0
 while cap.isOpened():
     ret, frame = cap.read()
-    frame_copy = frame.copy()
+    frame_all_bbox = frame.copy()
     # print(nr_frame)
     # nr_frame += 1
     # cv2.imshow('Frame', frame)
@@ -119,7 +185,7 @@ while cap.isOpened():
             for j in range(0, w - model_w, STRIDE):
                 if ruint8[i, j, 0] > PROB_THRESHOLD:
                     boxes.append([j, i, j+64, i+192])
-                    cv2.rectangle(img=frame_copy,
+                    cv2.rectangle(img=frame_all_bbox,
                                   pt1=(j, i),
                                   pt2=(j + 64, i + 192),
                                   color=(0, 0, 255),
@@ -127,7 +193,7 @@ while cap.isOpened():
                     # ruint8[i, j, :] = [0, 0, 255]
 
         boxes = np.array(boxes)
-        updated_boxes = non_max_suppression_fast(boxes, 0.1)
+        updated_boxes = non_max_suppression_fast(boxes, 0.4)
         for box in updated_boxes:
             cv2.rectangle(img=frame,
                           pt1=(box[0], box[1]),
@@ -135,10 +201,10 @@ while cap.isOpened():
                           color=(0, 0, 255),
                           thickness=2)
 
-        combined = np.hstack([frame_copy, frame])
+        combined = np.hstack([frame_all_bbox, frame])
         # cv2.imshow('Result', ruint8)
         # cv2.imshow('Frame', frame)
-        cv2.imshow('Combined', combined)
+        cv2.imshow('Left: all bboxes; right: suppressed', combined)
 
         key = cv2.waitKey(1)
         if key & 0xff == ord('q'):
