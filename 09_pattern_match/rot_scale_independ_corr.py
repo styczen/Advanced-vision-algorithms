7f7ab42e18bff88f58942add18269edfdafa370f
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2019-05-20 09:44:10 +0200

    Rot and scale independent pattern match finished

--- 09_pattern_match/rot_scale_independ_corr.py
+++ 09_pattern_match/rot_scale_independ_corr.py
@@ -1,8 +1,10 @@
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 import os
-import sys
+import matplotlib
+import matplotlib.pyplot as plt
+# import numpy.fft as fft
+from matplotlib.patches import Rectangle
 
 
 def hanning2D(n):
@@ -11,143 +13,152 @@ def hanning2D(n):
 
 
 def highpassFilter(size):
-    rows = np.cos(np.pi*np.matrix([-0.5 + x/(size[0]-1) for x in range(size[0])]))
-    cols = np.cos(np.pi*np.matrix([-0.5 + x/(size[1]-1) for x in range(size[1])]))
+    rows = np.cos(np.pi * np.matrix([-0.5 + x / (size[0] - 1) for x in range(size[0])]))
+    cols = np.cos(np.pi * np.matrix([-0.5 + x / (size[1] - 1) for x in range(size[1])]))
     X = np.outer(rows, cols)
-    return (1.0 - X)*(2.0 - X)
+    return (1.0 - X) * (2.0 - X)
 
 
-DIR = os.path.dirname(sys.argv[0])
+# def rotate(x, y, xo, yo, theta):  # rotate x,y around xo,yo by theta (deg)
+#     xr = np.cos(np.deg2rad(theta))*(x-xo)-np.sin(np.deg2rad(theta))*(y-yo) + xo
+#     yr = np.sin(np.deg2rad(theta))*(x-xo)+np.cos(np.deg2rad(theta))*(y-yo) + yo
+#     return int(xr), int(yr)
 
 
-print('Loading images...')
-pattern = cv2.imread(DIR + '/obrazy_Mellin/domek_r0_64.pgm', cv2.IMREAD_GRAYSCALE)
-test_img = cv2.imread(DIR + '/obrazy_Mellin/domek_r0.pgm', cv2.IMREAD_GRAYSCALE)
+plt.close('all')
+DIR = "/home/bartek/learning/Advanced-vision-algorithms/09_pattern_match"
+# DIR = os.getcwd()
+pattern_img = cv2.imread(DIR + '/obrazy_Mellin/domek_r0_64.pgm',
+                         cv2.IMREAD_GRAYSCALE)
+pattern_img_copy = pattern_img.copy()
 
-# Load color version of test image for more user friendly view of results
-test_img_color = cv2.imread(DIR + '/obrazy_Mellin/domek_r30.pgm')
-print('...done')
+test_img = cv2.imread(DIR + '/obrazy_Mellin/domek_s80.pgm',
+                      cv2.IMREAD_GRAYSCALE)
+pattern_img = pattern_img * hanning2D(pattern_img.shape[0])
 
-# Multiply pattern by hanning window
-pattern_win = pattern * hanning2D(pattern.shape[0])
+pattern = np.zeros(test_img.shape)
+pattern[0:pattern_img.shape[0], 0:pattern_img.shape[1]] = pattern_img
+offset = int((test_img.shape[0]-pattern_img.shape[0])//2.0)
+pattern_c = np.zeros(test_img.shape)
+pattern_c[offset:pattern_img.shape[0]+offset,
+          offset:pattern_img.shape[1]+offset] = pattern_img
 
-# Expand pattern to match test image size filling it with zeros
-ext_pattern = np.zeros(test_img.shape, dtype=np.uint8)
-ext_pattern[0:pattern_win.shape[0], 0:pattern_win.shape[1]] = pattern_win
+# 2D fft
+fft_pattern = np.fft.fft2(pattern)
+fft_pattern_sh = np.fft.fftshift(fft_pattern)
 
-# For display purposes expand pattern without hanning window applied
-ext_pattern1 = np.zeros(test_img.shape, dtype=np.uint8)
-ext_pattern1[0:pattern.shape[0], 0:pattern.shape[1]] = pattern
+fft_test = np.fft.fft2(test_img)
+fft_test_sh = np.fft.fftshift(fft_test)
 
-# FFT
-ext_pattern_fft = np.fft.fft2(ext_pattern)
-ext_pattern_fft = np.fft.fftshift(ext_pattern_fft)
+# Amplituda i filtracja
+highpass_filter = highpassFilter(pattern.shape)
+fft_pattern_abs = np.abs(fft_pattern_sh)*highpass_filter
+fft_domek_abs = np.abs(fft_test_sh)*highpass_filter
 
-test_img_fft = np.fft.fft2(test_img)
-test_img_fft = np.fft.fftshift(test_img_fft)
+# logPolar
+R = fft_pattern.shape[0] // 2.0
+M = 2 * R / np.log(R)
+pattern_polar = cv2.logPolar(src=fft_pattern_abs,
+                             center=(R, R),
+                             M=M,
+                             flags=cv2.INTER_LINEAR + cv2.WARP_FILL_OUTLIERS)
+domek_polar = cv2.logPolar(src=fft_domek_abs,
+                           center=(R, R),
+                           M=M,
+                           flags=cv2.INTER_LINEAR + cv2.WARP_FILL_OUTLIERS)
 
-# Amplitude
-ext_pattern_fft_abs = np.abs(ext_pattern_fft)
-test_img_fft_abs = np.abs(test_img_fft)
+# 2D fft
+fft_pattern_polar = np.fft.fft2(pattern_polar)
+fft_domek_polar = np.fft.fft2(domek_polar)
 
-# Highpass filtration
-ext_pattern_filtered = ext_pattern_fft_abs * highpassFilter(ext_pattern_fft_abs.shape)
-test_img_filtered = test_img_fft_abs * highpassFilter(test_img_fft_abs.shape)
+fft_pattern_polar = np.fft.fftshift(fft_pattern_polar)
+fft_domek_polar = np.fft.fftshift(fft_domek_polar)
 
-# Log-polar transform
-# Pattern
-middle_point = (ext_pattern_filtered.shape[0]//2, ext_pattern_filtered.shape[1]//2)
-M = 2 * ext_pattern_filtered.shape[0] / np.log(ext_pattern_filtered.shape[0])
-ext_pattern_logpolar = cv2.logPolar(ext_pattern_filtered, middle_point, M, cv2.INTER_LINEAR + cv2.WARP_FILL_OUTLIERS)
-
-# Test image
-middle_point = (test_img_filtered.shape[0]//2, test_img_filtered.shape[1]//2)
-M = 2 * test_img_filtered.shape[0] / np.log(test_img_filtered.shape[0])
-test_img_logpolar = cv2.logPolar(test_img_filtered, middle_point, M, cv2.INTER_LINEAR + cv2.WARP_FILL_OUTLIERS)
-
-# Log-polar FFT
-ext_pattern_lp_fft = np.fft.fft2(ext_pattern_logpolar)
-ext_pattern_lp_fft = np.fft.fftshift(ext_pattern_lp_fft)
-
-test_img_lp_fft = np.fft.fft2(test_img_logpolar)
-test_img_lp_fft = np.fft.fftshift(test_img_lp_fft)
-
-# Phase correlation
-R = np.conj(ext_pattern_lp_fft) * test_img_lp_fft
+# phase corr
+R = np.conj(fft_pattern_polar) * fft_domek_polar
 R = R / np.abs(R)
-ccor_norm = np.fft.ifft2(R)
-ccor_norm_abs = np.abs(ccor_norm)
+correl_norm = np.fft.ifft2(R)
 
-# Maximum in log-polar
-wsp_logr, wsp_kata = np.unravel_index(np.argmax(ccor_norm_abs), ccor_norm_abs.shape)
-
-# Calculate scaling and rotation from log-polar max
-rozmiar_logr = ext_pattern_logpolar.shape[0]
-rozmiar_kata = ext_pattern_logpolar.shape[1]
-
-if wsp_logr > rozmiar_logr//2:  # rozmiar_logr x rozmiar_kata to rozmiar obrazu logpolar
+# rot, scale
+wsp_kata, wsp_logr = np.unravel_index(np.argmax(np.abs(correl_norm)),
+                                      np.abs(correl_norm).shape)
+# rozmiar_logr x rozmiar_kata to rozmiar obrazu logpolar
+rozmiar_logr = domek_polar.shape[1]
+rozmiar_kata = domek_polar.shape[0]
+if wsp_logr > rozmiar_logr // 2:
     wykl = rozmiar_logr - wsp_logr  # powiekszenie
 else:
-    wykl = - wsp_logr               # pomniejszenie
-
-skala = np.exp(wykl / M)   # gdzie M to parametr funkcji cv2.logPolar, a wykl wyliczamy jako:
-A = (wsp_kata * 360.0) / rozmiar_kata
-kat1 = - A
+    wykl = - wsp_logr  # pomniejszenie
+A = (wsp_kata * 360.0)/rozmiar_kata
+# gdzie M to parametr funkcji cv2.logPolar, a wykl wyliczamy jako:
+scale = np.exp(wykl / M)
+kat1 = - A  # gdzie A = (wsp_kata * 360.0 ) /rozmiar_kata
 kat2 = 180 - A
 
-# Affine transform for both angles
-im = np.zeros(test_img.shape, dtype=np.uint8)
-middle = (im.shape[0]//2, im.shape[1]//2)
-pattern_middle = (pattern.shape[0]//2, pattern.shape[1]//2)
-im[middle[0]-pattern_middle[0]:middle[0]+pattern_middle[0], middle[1]-pattern_middle[1]:middle[1]+pattern_middle[1]] \
-    = pattern
+srodekTrans = ((pattern.shape[0] / 2), (pattern.shape[1] / 2))
+macierz_translacji_1 = cv2.getRotationMatrix2D((srodekTrans[0],
+                                                srodekTrans[1]), kat1, scale)
+macierz_translacji_2 = cv2.getRotationMatrix2D((srodekTrans[0],
+                                                srodekTrans[1]), kat2, scale)
 
-srodekTrans = (im.shape[0] / 2 - 0.5, im.shape[1] / 2 - 0.5)  # im to obraz wzorca uzupelniony zerami,
-# ale ze wzorcem umieszczonymna srodku, a nie w lewym, gornym rogu!
-macierz_translacji1 = cv2.getRotationMatrix2D(srodekTrans, kat1, skala)
-macierz_translacji2 = cv2.getRotationMatrix2D(srodekTrans, kat2, skala)
+im_rot_scaled_1 = cv2.warpAffine(pattern_c, macierz_translacji_1,
+                                 pattern.shape)
+im_rot_scaled_2 = cv2.warpAffine(pattern_c, macierz_translacji_2,
+                                 pattern.shape)
 
-obraz_obrocony_przeskalowany1 = cv2.warpAffine(im, macierz_translacji1, ext_pattern.shape)
-obraz_obrocony_przeskalowany2 = cv2.warpAffine(im, macierz_translacji2, ext_pattern.shape)
+# FFT2
+im_rot_scaled_fft_1 = np.fft.fft2(im_rot_scaled_1)
+im_rot_scaled_fft_2 = np.fft.fft2(im_rot_scaled_2)
 
-# FFT for transform imgs for both angles
-obraz_obrocony_przeskalowany1_fft = np.fft.fft2(obraz_obrocony_przeskalowany1)
-# obraz_obrocony_przeskalowany1_fft = np.fft.fftshift(obraz_obrocony_przeskalowany1_fft)
+im_rot_scaled_fft_1 = np.fft.fftshift(im_rot_scaled_fft_1)
+im_rot_scaled_fft_2 = np.fft.fftshift(im_rot_scaled_fft_2)
 
-obraz_obrocony_przeskalowany2_fft = np.fft.fft2(obraz_obrocony_przeskalowany2)
-# obraz_obrocony_przeskalowany2_fft = np.fft.fftshift(obraz_obrocony_przeskalowany2_fft)
+# Phase correlation
+R_1 = np.conj(im_rot_scaled_fft_1)*fft_test
+R_1 = R_1 / np.abs(R_1)
+ccor_norm_1 = np.fft.ifft2(R_1)
 
-# Phase correlation with test image
-R = np.conj(obraz_obrocony_przeskalowany1_fft) * test_img_fft
-R = R / np.abs(R)
-ccor_norm = np.fft.ifft2(R)
-ccor_norm_abs1 = np.abs(ccor_norm)
+R_2 = np.conj(im_rot_scaled_fft_2)*fft_test
+R_2 = R_2 / np.abs(R_2)
+ccor_norm_2 = np.fft.ifft2(R_2)
 
-R = np.conj(obraz_obrocony_przeskalowany2_fft) * test_img_fft
-R = R / np.abs(R)
-ccor_norm = np.fft.ifft2(R)
-ccor_norm_abs2 = np.abs(ccor_norm)
+y_norm_1, x_norm_1 = np.unravel_index(np.argmax(np.abs(ccor_norm_1)),
+                                      np.abs(ccor_norm_1).shape)
+y_norm_2, x_norm_2 = np.unravel_index(np.argmax(np.abs(ccor_norm_2)),
+                                      np.abs(ccor_norm_2).shape)
 
-# Find greater correlation between both angles
-if np.max(ccor_norm_abs1) >= np.max(ccor_norm_abs2):
-    y, x = np.unravel_index(np.argmax(ccor_norm_abs1), ccor_norm_abs1.shape)
+if np.max(np.abs(ccor_norm_1)) > np.max(np.abs(ccor_norm_2)):
+    x_best = x_norm_1
+    y_best = y_norm_1
+    kat_best = kat1
+    pattern_best = im_rot_scaled_1
+    corr_best = ccor_norm_1
+    macierz_translacji_best = macierz_translacji_1
 else:
-    y, x = np.unravel_index(np.argmax(ccor_norm_abs2), ccor_norm_abs2.shape)
+    x_best = x_norm_2
+    y_best = y_norm_2
+    kat_best = kat2
+    pattern_best = im_rot_scaled_2
+    corr_best = ccor_norm_2
+    macierz_translacji_best = macierz_translacji_2
 
-# Draw found maximum
-cv2.circle(img=test_img_color,
-           center=(x, y),
-           radius=1,
-           color=(0, 255, 0),
-           thickness=3)
+M = cv2.getRotationMatrix2D((test_img.shape[0]//2,
+                             test_img.shape[1]//2), -kat_best, 1)
+test_img = cv2.warpAffine(test_img, M,
+                          test_img.shape)
 
-# Transform pattern to match the place in image from test image
-tr_m = np.float32([[1, 0, x], [0, 1, y]])
-ext_pattern_tr = cv2.warpAffine(ext_pattern, tr_m, (ext_pattern.shape[1], ext_pattern.shape[0]))
+if abs(kat_best) < 1:
+    y_best = y_best - 128
+    M = np.float32([[1, 0, -x_best], [0, 1, -y_best]])
+    test_img = cv2.warpAffine(test_img, M,
+                              test_img.shape)
 
 plt.figure()
-plt.imshow(ext_pattern_tr, cmap='gray')
+plt.imshow(pattern_img_copy, cmap='gray')
 
 plt.figure()
-plt.imshow(test_img_color)
+plt.imshow(test_img, 'gray')
+
+# print('x = {}\ny = {}\nangle = {}'.format(x_best, y_best, kat_best))
+
 plt.show()
