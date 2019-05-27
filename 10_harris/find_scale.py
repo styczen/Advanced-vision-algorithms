a82feaa557e96ece84602ae151e51b91733a3e23
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2019-05-27 09:43:23 +0200

    Find common scale debuged, still no good results

--- 10_harris/find_scale.py
+++ 10_harris/find_scale.py
@@ -38,15 +38,16 @@ def pyramid(image, blur_nbr, k, sigma):
     prev_img = cv2.GaussianBlur(fimage, (0, 0), sigmaX=sigma, sigmaY=sigma)
 
     for i in range(1, blur_nbr+1):
-        blurred = cv2.GaussianBlur(image, (0, 0), sigmaX=k**i*sigma, sigmaY=k**i*sigma)
+        blurred = cv2.GaussianBlur(
+            image, (0, 0), sigmaX=k**i*sigma, sigmaY=k**i*sigma)
         diff = np.float64(blurred) - np.float64(prev_img)
         diff = diff/np.max(diff)
-        res_img[i-1, :, :] = diff
+        res_img[i-1] = diff
         prev_img = blurred
 
     return res_img
 
-# FIXME: Find max call for whole pyramid, find max in all 3 dimensions
+
 def find_max(image, size, threshold):
     """Finds maximum of array"""
     data_max = filters.maximum_filter(image, size)
@@ -56,24 +57,22 @@ def find_max(image, size, threshold):
     return np.nonzero(maxima)
 
 
-def pyramid_find_max(pyramid_imgs, size, threshold):
-    """Finds maximum for every scale in pyramid."""
-    res = []
-    for i in range(pyramid_imgs.shape[0]):
-        res.append(find_max(pyramid_imgs[i, :, :], size, threshold))
-    return res
-
-
 def draw_points(img, points):
     plt.figure()
     plt.imshow(img, cmap='gray')
     plt.plot(points[1], points[0], '*', color='r')
 
 
-def pyramid_draw_points(pyramid_imgs, pts):
-    for i in range(pyramid_imgs.shape[0]):
-        draw_points(pyramid_imgs[i, :, :], pts[i])
-    # plt.show()
+def draw_pyramids(pyramid, pts, nr):
+    for i in range(pyramid.shape[0]):
+        x = pts[2]  # wspolrzedne x we wszystkich skalach
+        x = x[pts[0] == i]  # jako wspolrzedne x tylko w skali i
+        y = pts[1]  # wspolrzedne x we wszystkich skalach
+        y = y[pts[0] == i]  # jako wspolrzedne x tylko w skali i
+        plt.figure()
+        plt.imshow(pyramid[i], 'gray')
+        plt.scatter(x=x, y=y, marker='*', c='r')
+        plt.title('{}, scale = {}'.format(nr, i))
 
 
 # Loading images
@@ -87,23 +86,47 @@ img1 = cv2.imread(DIR + '/pliki_harris/' + IMG_NAME1, cv2.IMREAD_GRAYSCALE)
 img2 = cv2.imread(DIR + '/pliki_harris/' + IMG_NAME2, cv2.IMREAD_GRAYSCALE)
 
 p1 = pyramid(img1, 5, 1.26, 1.6)
-e1 = find_max(p1, 7, 0.5)
-
 p2 = pyramid(img2, 10, 1.26, 1.6)
+
+e1 = find_max(p1, 7, 0.5)
 e2 = find_max(p2, 7, 0.5)
 
-pyramid_draw_points(p1, e1)
-pyramid_draw_points(p2, e2)
+draw_pyramids(p1, e1, 1)
+draw_pyramids(p2, e2, 2)
+
+# for i in range(5):
+#     x = e1[2]  # wspolrzedne x we wszystkich skalach
+#     x = x[e1[0] == i]  # jako wspolrzedne x tylko w skali i
+#     y = e1[1]  # wspolrzedne x we wszystkich skalach
+#     y = y[e1[0] == i]  # jako wspolrzedne x tylko w skali i
+#     plt.figure()
+#     plt.imshow(p1[i], 'gray')
+#     plt.scatter(x=x, y=y, marker='*', c='r')
+#     plt.title('img 1 scale='+str(i))
+#     # plt.show()
+# for i in range(10):
+#     x = e2[2]  # wspolrzedne x we wszystkich skalach
+#     x = x[e2[0] == i]  # jako wspolrzedne x tylko w skali i
+#     y = e2[1]  # wspolrzedne x we wszystkich skalach
+#     y = y[e2[0] == i]  # jako wspolrzedne x tylko w skali i
+#     plt.figure()
+#     plt.imshow(p2[i], 'gray')
+#     plt.scatter(x=x, y=y, marker='*', c='r')
+#     plt.title('img 2 scale='+str(i))
+plt.show()
+
+# pyramid_draw_points(p1, e1)
+# pyramid_draw_points(p2, e2)
 # plt.show()
 # 10 scales
 
-# Use Harris for both two images
-h1 = h_fun(img1, KERNEL_SIZE)
-m1 = find_max(h1, KERNEL_SIZE, THRESHOLD)
+# # Use Harris for both two images
+# h1 = h_fun(img1, KERNEL_SIZE)
+# m1 = find_max(h1, KERNEL_SIZE, THRESHOLD)
 
-h2 = h_fun(img2, KERNEL_SIZE)
-m2 = find_max(h2, KERNEL_SIZE, THRESHOLD)
+# h2 = h_fun(img2, KERNEL_SIZE)
+# m2 = find_max(h2, KERNEL_SIZE, THRESHOLD)
 
-draw_points(img1, m1)
-draw_points(img2, m2)
+# draw_points(img1, m1)
+# draw_points(img2, m2)
 plt.show()
